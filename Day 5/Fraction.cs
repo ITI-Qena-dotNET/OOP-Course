d846181f4e026cc65a464133c7da2fad18beb155
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-12-29 15:12:28 +0200

    Added Lab 5 Task Solution

---
+++ Day 5/Fraction.cs
@@ -0,0 +1,118 @@
+﻿namespace ClassesLab;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    /// <summary>
+    /// Default Constructor
+    /// </summary>
+    /// <param name="numerator"></param>
+    /// <param name="denomerator"></param>
+    public Fraction(int numerator, int denomerator)
+    {
+        _numerator = numerator;
+        _denominator = denomerator;
+    }
+
+    /// <summary>
+    /// Copy Constructor
+    /// </summary>
+    /// <param name="instance"></param>
+    public Fraction(Fraction instance)
+    {
+        _numerator = instance._numerator;
+        _denominator = instance._denominator;
+    }
+
+    public int Numerator
+    {
+        get { return _numerator; }
+        set { _numerator = value; }
+    }
+
+    public int Denomerator
+    {
+        get { return _denominator; }
+        set { _denominator = value; }
+    }
+
+    public override string ToString() 
+        => $"{_numerator}/{_denominator}";
+
+    //public string PrintFraction() 
+    //    => $"{_numerator}/{Denomerator}";
+
+    /// <summary>
+    /// Gets the Greatest Common Divisor (GCD) for 2 integer numbers
+    /// </summary>
+    /// <param name="num1">First integer</param>
+    /// <param name="num2">Second integer</param>
+    /// <returns>The Greatest Common Divisor (GCD) of num1 and num2</returns>
+    private static int GetGreatestCommonDivisor(int num1, int num2)
+    {
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 > num2)
+                num1 %= num2;
+            else
+                num2 %= num1;
+        }
+
+        return num1 | num2;
+    }
+
+    public static Fraction operator +(Fraction first, Fraction second)
+    {
+        int resultNumerator;
+
+        if (first.Denomerator == second.Denomerator)
+        {
+            resultNumerator = first.Numerator + second.Numerator;
+
+            return new(resultNumerator, first.Denomerator);
+        }
+
+        Fraction f1 = new(first);
+        int oldDenominator = f1.Denomerator;
+        Fraction f2 = new(second);
+
+        f1.Numerator *= f2.Denomerator;
+        f1.Denomerator *= f2.Denomerator;
+
+        f2.Numerator *= oldDenominator;
+        f2.Denomerator *= oldDenominator;
+
+        resultNumerator = f1.Numerator + f2.Numerator;
+        int gcd = GetGreatestCommonDivisor(resultNumerator, f1.Denomerator);
+
+        return new(resultNumerator / gcd, f1.Denomerator / gcd);
+    }
+
+    public static Fraction operator ++(Fraction input)
+    {
+        Fraction f = new(input.Denomerator, input.Denomerator);
+
+        Fraction result = input + f;
+
+        return result;
+    }
+
+    public static bool operator ==(Fraction first, Fraction second)
+    {
+        int firstGCD = GetGreatestCommonDivisor(first.Numerator, first.Denomerator);
+        int secondGCD = GetGreatestCommonDivisor(second.Numerator, second.Denomerator);
+
+        if (first.Numerator == second.Numerator && first.Denomerator == second.Denomerator)
+            return true;
+        else if (first.Numerator / firstGCD == second.Numerator / secondGCD &&
+            first.Denomerator / firstGCD == second.Denomerator / secondGCD)
+            return true;
+
+        return false;
+    }
+
+    public static bool operator !=(Fraction first, Fraction second)
+        => !(first == second);
+}
---
+++ Day 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClassesLab;
+
+Console.WriteLine("*********************** Lab 5 Problem ***********************");
+
+/*
+ * Create a Fraction class to implement fraction mathematics
+ * 
+ * Class has 2 members: Numerator & Denominator
+ * 
+ * Functions to implement (Operator Overloaded):
+ * 
+ * 1) + (Addition of 2 fractions)
+ * 2) ++ (Incrementing a fraction by 1)
+ * 3) == (Comparing 2 fractions for similarity)
+ * 4) Print (To show fraction as "x/y")
+ * 5) Copy Constructor
+ */
+
+Fraction f1 = new(1, 2);
+Fraction f2 = new(3, 5);
+
+Console.WriteLine($"Fraction 1: {f1} \nFraction 2: {f2}\n");
+
+Fraction addResult = f1 + f2;
+Console.WriteLine($"Add Result is {addResult}");
+
+Fraction incrementResult = (f2++);
+Console.WriteLine($"Increment Result is {incrementResult}");
+
+Fraction f3 = new(2, 4);
+Fraction f4 = new(3, 4);
+
+bool isEqual1 = f1 == f3;
+bool isEqual2 = f1 == f4;
+Console.WriteLine($"Equality 1 Result is {isEqual1}");
+Console.WriteLine($"Equality 2 Result is {isEqual2}");
+
+bool isNotEqual1 = f1 != f3;
+bool isNotEqual2 = f1 != f4;
+Console.WriteLine($"Non-Equality 1 Result is {isNotEqual1}");
+Console.WriteLine($"Non-Equality 2 Result is {isNotEqual2}");
