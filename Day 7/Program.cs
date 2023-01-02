b77d306d065b4197cc66c42161d4ce47af32ca9a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-01-02 15:11:53 +0200

    Add Lab 7 Task Solution

---
+++ Day 7/Program.cs
@@ -0,0 +1,20 @@
+﻿using PolymorphismLab;
+
+Console.WriteLine("*********************** Lab 7 Problem ***********************");
+
+/*
+ * Modify Problem 1 in Lab 6 to make "Shape" Class Abstract
+ * and make CalcArea() abstract
+ * 
+ * 
+ * Create Class "Picture" and add shapes array (as aggreggate)
+ * and calculate total area in a function
+ * 
+ */
+
+Picture pic = new()
+{
+    Shapes = new Shape[] { new Square(2), new Rectangle(2, 3), new Circle(2) }
+};
+
+Console.WriteLine($"The total area of shapes inside picture is {pic.CalculateTotalArea()}");
---
+++ Day 7/Shape.cs
@@ -0,0 +1,63 @@
+﻿namespace PolymorphismLab;
+
+public sealed class Picture
+{
+    public Shape[] Shapes { get; set; }
+
+    public double CalculateTotalArea()
+    {
+        double totalArea = 0;
+
+        foreach (Shape sh in Shapes)
+        {
+            totalArea += sh.CalculateArea();
+        }
+
+        return totalArea;
+    }
+}
+
+public abstract class Shape
+{
+    /// <summary>
+    /// The base dimension for any shape.
+    /// Considered as Length in case of rectangle and Side in case of Square and Radius in case of Circle
+    /// </summary>
+    public int Dimension { get; set; }
+
+    protected Shape(int dimension) 
+        => Dimension = dimension;
+
+    public abstract double CalculateArea();
+}
+
+public class Square : Shape
+{
+    public Square(int dimension) : base(dimension)
+    {
+    }
+
+    public override double CalculateArea()
+        => Math.Pow(Dimension, 2);
+}
+
+public class Rectangle : Shape
+{
+    public Rectangle(int dimension, int secondDimension) : base(dimension)
+        => SecondDimension = secondDimension;
+
+    public int SecondDimension { get; set; }
+
+    public override double CalculateArea()
+        => Dimension * SecondDimension;
+}
+
+public class Circle : Shape
+{
+    public Circle(int dimension) : base(dimension)
+    {
+    }
+
+    public override double CalculateArea()
+        => Math.PI * Math.Pow(Dimension, 2);
+}
